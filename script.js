a9e81faac215f606d4507ee92afaed130175c683
Heikki Kantanen <hessu.kantanen@gmail.com>
2023-01-16 18:44:54 +0200

    updated the look

--- script.js
+++ script.js
@@ -6,43 +6,44 @@ const linkWrapper = document.querySelector(".link-wrapper");
 const menuIcon = document.querySelector(".menu-icon");
 const links = document.querySelector(".link");
 
-
 window.onscroll = function () {
-  scrollFunction();
+	scrollFunction();
 };
 
 const scrollFunction = () => {
-    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-      document.querySelector(".main_nav").style.backgroundColor = "white";
-      document.querySelector(".heikki").style.color = "black";
-      document.querySelector(".link").style.color = "black";
-      document.querySelector(".link2").style.color = "black";
-      document.querySelector(".link3").style.color = "black";
-      document.querySelector(".link4").style.color = "black";
-      document.querySelector(".link5").style.color = "black";
-      document.querySelector(".link6").style.color = "black";
-      document.querySelector(".p").style.color = "black";
-      document.querySelector(".p2").style.color = "black";
-      document.querySelector(".p3").style.color = "black";
-      document.querySelector(".p4").style.color = "black";
-      document.querySelector(".p5").style.color = "black";
-    
-      } else {
-      document.querySelector(".main_nav").style.backgroundColor = "transparent";
-      document.querySelector(".heikki").style.color = "white";
-      document.querySelector(".link").style.color = "white";
-      document.querySelector(".link2").style.color = "white";
-      document.querySelector(".link3").style.color = "white";
-      document.querySelector(".link4").style.color = "white";
-      document.querySelector(".link5").style.color = "white";
-      document.querySelector(".link6").style.color = "white";
-      document.querySelector(".p").style.color = "white";
-      document.querySelector(".p2").style.color = "white";
-      document.querySelector(".p3").style.color = "white";
-      document.querySelector(".p4").style.color = "white";
-      document.querySelector(".p5").style.color = "white";
-      }
-  };
+	if (
+		document.body.scrollTop > 200 ||
+		document.documentElement.scrollTop > 200
+	) {
+		document.querySelector(".main_nav").style.backgroundColor = "white";
+		document.querySelector(".heikki").style.color = "black";
+		document.querySelector(".link").style.color = "black";
+		document.querySelector(".link2").style.color = "black";
+		document.querySelector(".link3").style.color = "black";
+		document.querySelector(".link4").style.color = "black";
+		// document.querySelector(".link5").style.color = "black";
+		// document.querySelector(".link6").style.color = "black";
+		document.querySelector(".p").style.color = "black";
+		document.querySelector(".p2").style.color = "black";
+		document.querySelector(".p3").style.color = "black";
+		// document.querySelector(".p4").style.color = "black";
+		// document.querySelector(".p5").style.color = "black";
+	} else {
+		document.querySelector(".main_nav").style.backgroundColor = "transparent";
+		document.querySelector(".heikki").style.color = "white";
+		document.querySelector(".link").style.color = "white";
+		document.querySelector(".link2").style.color = "white";
+		document.querySelector(".link3").style.color = "white";
+		document.querySelector(".link4").style.color = "white";
+		// document.querySelector(".link5").style.color = "white";
+		// document.querySelector(".link6").style.color = "white";
+		document.querySelector(".p").style.color = "white";
+		document.querySelector(".p2").style.color = "white";
+		document.querySelector(".p3").style.color = "white";
+		// document.querySelector(".p4").style.color = "white";
+		// document.querySelector(".p5").style.color = "white";
+	}
+};
 
 //   if (window.innerWidth > 450) {
 // 	window.onscroll = function () {
@@ -65,7 +66,6 @@ menuIcon.addEventListener("click", toggleMenu);
 
 links.forEach((link) => link.addEventListener("click", toggleMenu));
 
-function toggleMenu () {
-    linkWrapper.classList.toggle("show");
+function toggleMenu() {
+	linkWrapper.classList.toggle("show");
 }
-
